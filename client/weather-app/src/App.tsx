8382c2b6de15303d7d949f97b0ccd759b5c4793a
Sly <Sly@VM-WIN10-DEV.local>
2020-10-30 19:31:33 +1000

    Client side button to trigger the fetch

--- client/weather-app/src/App.tsx
+++ client/weather-app/src/App.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Weather from './components/Weather'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <Weather></Weather>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
---
+++ client/weather-app/src/components/Weather.tsx
@@ -0,0 +1,65 @@
+import React, { PureComponent } from "react";
+
+export interface Forecast {
+    date: string;
+    temperatureC: string;
+    temperatureF: string;
+    summary: string;    
+}
+
+class WeatherComponent extends PureComponent<{}, { count: number, forecasts: Forecast[] }> {
+  state = { count: 0, forecasts: [] };
+  updateCount = (e: React.MouseEvent<HTMLButtonElement>): void => {
+    switch (e.currentTarget.innerText) {
+      case "-":
+        this.setState({ count: this.state.count - 1 });
+        break;
+      case "+":
+      default:
+        this.setState({ count: this.state.count + 1 });
+        break;
+    }
+  };
+  populateWeatherData = async () => {
+    const response = await fetch('weatherforecast');
+    const data = await response.json();
+    this.setState({ forecasts: data });
+  }
+  static renderForecastsTable(forecasts: Forecast[]) {
+    return (
+      <table className='table table-striped' aria-labelledby="tabelLabel">
+        <thead>
+          <tr>
+            <th>Date</th>
+            <th>Temp. (C)</th>
+            <th>Temp. (F)</th>
+            <th>Summary</th>
+          </tr>
+        </thead>
+        <tbody>
+          {forecasts.map(forecast =>
+            <tr key={forecast.date}>
+              <td>{forecast.date}</td>
+              <td>{forecast.temperatureC}</td>
+              <td>{forecast.temperatureF}</td>
+              <td>{forecast.summary}</td>
+            </tr>
+          )}
+        </tbody>
+      </table>
+    );
+  }
+  render(): JSX.Element {
+    const { count, forecasts } = this.state;
+    let contents = forecasts.length === 0 ? <p>Ready</p>
+      : WeatherComponent.renderForecastsTable(this.state.forecasts);
+    return (
+      <div>
+        <h2>Weather Component</h2>
+        <button onClick={this.populateWeatherData}>Fetch</button>
+        {contents}
+      </div>
+    );
+  }
+}
+export default WeatherComponent;
