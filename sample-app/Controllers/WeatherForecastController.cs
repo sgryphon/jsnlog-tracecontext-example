9e406d97c5448de96223159e032722494b72f909
Sly <Sly@VM-WIN10-DEV.local>
2020-10-30 18:03:47 +1000

    Add log to data request

--- sample-app/Controllers/WeatherForecastController.cs
+++ sample-app/Controllers/WeatherForecastController.cs
@@ -26,6 +26,8 @@ namespace sample_app.Controllers
         [HttpGet]
         public IEnumerable<WeatherForecast> Get()
         {
+            Log.WeatherRequested(_logger, null);
+            
             var rng = new Random();
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
---
+++ sample-app/Log.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.Extensions.Logging;
+
+namespace sample_app
+{
+    internal static class Log
+    {
+        // Event IDs: ABxx (based on Theory of Reply Codes)
+        
+        // Event ID Type:
+        // 1bxx info - preliminary
+        // 2bxx info - completion
+        // 3bxx info - intermediate
+        // 8bxx info - finalization
+        // 4bxx warning
+        // 5bxx error
+        // 9bxx critical
+        
+        // Event ID Category:
+        // a0xx syntax
+        // a1xx information
+        // a2xx connections
+        // a3xx storage
+        
+        // 1bxx 
+        
+        public static readonly Action<ILogger, Exception?> WeatherRequested =
+            LoggerMessage.Define(LogLevel.Information,
+                new EventId(1001, nameof(WeatherRequested)),
+                "Weather requested");      
+    }
+}
